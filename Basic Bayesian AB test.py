a7bf5d1440f261fedd9d0cfe80402ee89ac31cc8
Richie-Lee <73502276+Richie-Lee@users.noreply.github.com>
2023-07-20 13:30:56 +0200

    Added Likelihood function implementation
    tests non-composite H1 vs H0
    
    Bayes factor (H1/H0)

--- Basic Bayesian AB test.py
+++ Basic Bayesian AB test.py
@@ -12,12 +12,13 @@ from datetime import datetime
 # Track total runtime
 _start_time = datetime.now()
 
-
 """
 Part 0: Settings & Hyperparameters
 """
-C = {"n": 1000, "true_prob": 0.5} # control
-T = {"n": 1000, "true_prob": 0.55} # treatment
+# H0: effect = 0, H1: effect = mde (note, not composite! though still practical for that purpose)
+hypotheses = {"null": 0.5, "alt": 0.52}
+C = {"n": 1_000, "true_prob": 0.5} # control
+T = {"n": 1_000, "true_prob": 0.55} # treatment
 
 
 
@@ -41,9 +42,40 @@ def get_bernoulli_sample(mean, n):
     
     return data, converted, mean 
 
-C["sample"], C["converted"], C["sample_mean"] = get_bernoulli_sample(mean = C["true_prob"], n = C["n"])
-T["sample"], T["converted"], T["sample_mean"] = get_bernoulli_sample(mean = T["true_prob"], n = T["n"])
+C["sample"], C["converted"], C["sample_conversion_rate"] = get_bernoulli_sample(mean = C["true_prob"], n = C["n"])
+T["sample"], T["converted"], T["sample_conversion_rate"] = get_bernoulli_sample(mean = T["true_prob"], n = T["n"])
 
+
+
+"""
+Part 2: Likelihoods
+"""
+
+def bernoulli_likelihood(hypothesis, outcomes):
+    likelihood = 1.0
+    for y in outcomes:
+        if y == 1:
+            likelihood *= hypothesis
+        elif y == 0:
+            likelihood *= (1 - hypothesis)
+        else:
+            raise ValueError("Outcomes must contain non-negative integers")
+
+    return likelihood
+
+def likelihood_ratio_test(treatment):
+    # Get likelihoods
+    null_likelihood = bernoulli_likelihood(hypotheses["null"], treatment)
+    alt_likelihood = bernoulli_likelihood(hypotheses["alt"], treatment)
+
+    # Compute Bayes Factor: p(data|H1) / p(data|H0)
+    bayes_factor = alt_likelihood / null_likelihood
+
+    return bayes_factor, alt_likelihood, null_likelihood
+
+T["bayes_factor"], T["likelihood_H1"], T["likelihood_H0"] = likelihood_ratio_test(T["sample"]["converted"])
+
+Prob_H1 = round(T["bayes_factor"] / (T["bayes_factor"] + 1) * 100, 1)
 
 
 
