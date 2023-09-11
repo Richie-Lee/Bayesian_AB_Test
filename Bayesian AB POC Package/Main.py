3ebd21c1c1b5f6432ad5ec20c6c1c6526c0eeec0
Richie-Lee <73502276+Richie-Lee@users.noreply.github.com>
2023-09-11 16:34:48 +0200

    Added new classes: DGP & Prior (beta)

--- Bayesian AB POC Package/Main.py
+++ Bayesian AB POC Package/Main.py
@@ -2,7 +2,7 @@
 User input
 """ 
 
-# Specify DGP - [n, TE]
+# Specify DGP
 
 # Specify Prior odds - [odds]
 
@@ -40,3 +40,42 @@ REPEAT
 # Visualistion
 
 """ 
+
+import pandas as pd
+import random
+from datetime import datetime
+
+import part_1_dgp as p1_dgp
+import part_2_prior as p2_prior
+
+# Define Control & Treatment DGP (Bernoulli distributed)
+C = {"n": 10_000, "true_prob": 0.5}
+T = {"n": 10_000, "true_prob": 0.53}
+
+""" 
+Part 1: Generate data
+""" 
+
+C["sample"], C["converted"], C["sample_conversion_rate"] = p1_dgp.get_bernoulli_sample(mean = C["true_prob"], n = C["n"])
+T["sample"], T["converted"], T["sample_conversion_rate"] = p1_dgp.get_bernoulli_sample(mean = T["true_prob"], n = T["n"])
+
+
+""" 
+Part 2: Prior
+""" 
+
+prior_odds = 1 
+
+
+prior_type = "beta"
+prior_parameters = {
+    "beta" : {"T_prior_prob" : 0.5, "T_weight" : 1000, "C_prior_prob" : 0.52,"C_weight" : 1000},
+    "right haar" : {"param":None}
+    }
+
+prior_calculator = p2_prior.get_prior(prior_type, prior_parameters[prior_type])
+C_prior, T_prior = prior_calculator.get_values()
+
+
+
+
---
+++ Bayesian AB POC Package/part_2_prior.py
@@ -0,0 +1,29 @@
+class get_prior():
+    def __init__(self, prior_type, prior_parameters):
+        self.prior_type = prior_type
+        self.parameters = prior_parameters
+        
+        # Execute main method
+        self.get_values()
+        
+    def beta_prior(self, parameters):
+        # Sample from Beta(weight(prior belief) + 1, weight(1 - prior belief) + 1)       
+        T_alpha = round(parameters["T_prior_prob"], 2) * parameters["T_weight"] + 1
+        T_beta = (1 - round(parameters["T_prior_prob"], 2)) * parameters["T_weight"] + 1
+        C_alpha = round(parameters["C_prior_prob"], 2) * parameters["C_weight"] + 1
+        C_beta = (1 - round(parameters["C_prior_prob"], 2)) * parameters["C_weight"] + 1
+        
+        # Collect and store parameters
+        T_prior = {"alpha": T_alpha, "beta": T_beta}
+        C_prior = {"alpha": C_alpha, "beta": C_beta}
+        return C_prior, T_prior
+    
+    def get_values(self):
+        if self.prior_type == "beta":
+            C_prior, T_prior = self.beta_prior(self.parameters)
+            
+        # Raise ValueError for unsupported/invalid prior types
+        else:
+            raise ValueError(f"the '{self.prior_type}' prior is not supported by this implementation of Bayesian A/B testing")
+        
+        return C_prior, T_prior
