f24c1423e268a0f8361abf1d9e2c0cfdf9cef2a5
Richie-Lee <73502276+Richie-Lee@users.noreply.github.com>
2023-09-18 15:41:29 +0200

    Early stopping implementation

--- Bayesian AB POC Package/part_3_bayes_factors.py
+++ Bayesian AB POC Package/part_3_bayes_factors.py
@@ -2,24 +2,23 @@ import numpy as np
 from scipy.special import betaln
 
 class get_bayes_factor():
-    def __init__(self, T, T_prior, C, C_prior, prior_type):
+    def __init__(self, T, T_prior, C, C_prior, prior_type, early_stopping_settings):
         self.T, self.C = T, C
         self.prior_type = prior_type
         self.T_prior, self.C_prior = T_prior, C_prior
+        self.es_settings = early_stopping_settings
         
         # Execute main method
         self.get_values()
 
-    def beta_bf(self, T, C, T_prior, C_prior):    
+
+
+    def beta_bf(self, c_t, n_t, c_c, n_c, T_prior, C_prior):    
         # Unpack prior values
         alpha_0, beta_0 = C_prior["alpha"], C_prior["beta"] # prior under H0 (control in this design)
         alpha_t, beta_t = T_prior["alpha"], T_prior["beta"] # prior under Treatment
         alpha_c, beta_c = C_prior["alpha"], C_prior["beta"] # prior under Control
         
-        # Unpack outcomes
-        c_t, n_t = T["converted"], T["n"]  # Successes and total observations for treatment group
-        c_c, n_c = C["converted"], C["n"]  # Successes and total observations for control group
-
         # Log probability of data under H0
         log_prob_data_H0 = betaln(alpha_0 + c_t + c_c, beta_0 + n_t + n_c - c_t - c_c) - betaln(alpha_0, beta_0)
     
@@ -36,9 +35,46 @@ class get_bayes_factor():
         return bf
     
     def get_values(self):
+        """
+        Fixed Horizon
+        """
+        c_t, n_t = self.T["converted"], self.T["n"]  # Successes and total observations for treatment group
+        c_c, n_c = self.C["converted"], self.C["n"]  # Successes and total observations for control group
+        
         if self.prior_type == "beta":
-            bf = self.beta_bf(self.T, self.C, self.T_prior, self.C_prior)
+            bf_fixed_horizon = self.beta_bf(c_t, n_t, c_c, n_c, self.T_prior, self.C_prior)
+        
+        
+        """
+        Early Stopping
+        """
+        n_observed, interim_tests = 0, []
+        
+        # Convert probability to odds (Bayes factors stopping rule)
+        k = (self.es_settings["prob_early_stopping"] * 100) / (100 - (self.es_settings["prob_early_stopping"] * 100))
+        
+        while n_observed <= self.T["n"]:
+            # Mask observations to resemble partial sampling
+            c_t, n_t = sum(self.T["sample"][:n_observed]), n_observed
+            c_c, n_c = sum(self.C["sample"][:n_observed]), n_observed
             
-        return bf
+            if self.prior_type == "beta":
+                bf = self.beta_bf(c_t, n_t, c_c, n_c, self.T_prior, self.C_prior)
+            
+            # Store results
+            interim_tests.append((n_observed, bf))
+            
+            # option to print progress if desired
+            if self.es_settings["print_progress"] == True:
+                print(f"n: {n_observed}/{self.T['n']}, BF: {bf}")
+          
+            # Stopping criteria
+            if (bf > k or bf < 1/k):
+                break
+
+            # Extend sample & get conversions
+            n_observed += self.es_settings["interim_test_interval"]
+        
+        return bf_fixed_horizon, interim_tests, k
         
         
